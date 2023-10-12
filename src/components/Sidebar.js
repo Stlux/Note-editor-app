b6b3d07faa1d761234127ec51e17b209ca218fa3
Kyrylo <kirilllikholetov3@gmail.com>
2023-10-12 21:10:44 +0200

    v.0.9

---
+++ src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default function Sidebar(props) {
+    const noteElements = props.notes.map((note, index) => (
+        <div key={note.id}>
+            <div id="note-elem"
+                
+                className={`title ${
+                    note.id === props.currentNote.id ? "selected-note" : ""
+                }`}
+                onClick={() => props.setCurrentNoteId(note.id)}
+            >
+                <h4 className="text-snippet">📝 {note.body.split("\n")[0]}</h4> 
+                
+            </div>
+        </div>
+    ))
+
+    return (
+        <section className="pane sidebar">
+            <div className="sidebar--header">
+                <h3>Note App 🖌️</h3>
+                <button className="new-note" onClick={props.newNote}>+</button>
+            </div>
+            {noteElements}
+        </section>
+    )
+}
